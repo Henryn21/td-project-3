97228850d1fc89cd49e63c6a26901f715dc21796
Henry <45953538+Henryn21@users.noreply.github.com>
2020-04-13 13:24:39 -0700

    added t shirt section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //set focus to first field
 document.querySelector("input").focus();
-//job role section
+
+//Job role section
+
 //"other" title hidden/visible
 const other=document.querySelector("[value=other]");
 const hidden=document.querySelector("#other-title");
@@ -15,3 +17,38 @@ titleList.addEventListener("change", (e)=>{
         hidden.style.display="none";
     }
 });
+
+//T shirt menu section
+
+//variables for color list and design list
+const designList= document.querySelector("#design");
+const colorList=document.querySelector("#color");
+
+//initially hide colors
+for(let i=0;i<colorList.length;i++){
+    colorList[i].style.display="none";
+}
+//set placeholder
+const placeholder= document.createElement('option');
+placeholder.textContent="Please select a T-shirt theme";
+placeholder.selected=true;
+colorList.appendChild(placeholder);
+
+//add listner to elements in design menu
+designList.addEventListener("change",(e)=>{
+    //if design selected, show colors
+    if(event.target.value=="js puns"||event.target.value=="heart js"){
+        for(let i=0;i<colorList.length;i++){
+            colorList[i].style.display="block";
+        }
+        colorList.removeChild(placeholder);
+    }
+    else{
+        for(let i=0;i<colorList.length;i++){
+            colorList[i].style.display="none";
+        }
+        colorList.appendChild(placeholder);
+    }
+});
+
+//
